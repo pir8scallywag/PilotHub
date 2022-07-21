6daa316df515ea895cd5dea3f70f41b93a0c0daf
pir8scallywag <jameskinlaw78@yahoo.com>
2022-07-21 18:22:04 -0400

    Initial

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # PilotHub
-Local Brewery finder for off duty Pilots
+
+## Description
+Are you a pilot?  If so, PilotHub was built to become your new wingman and render a first class app experience that loads critical airport and nearest brewery information faster than anything you're flying these days!  We called it hub to stay trendy, but you might think of it as one of those fancy airport lounges. And remember, "8 hours bottle to throttle!"
+
+## Languages
+* HTML
+* CSS
+* JavaScript
+
+** Contributors
+Jon Bullin
+Erik Frank
+Meghan Harper
+Brian King
+James Kinlaw
+Nicholas Zervos
+
+** URL
+Deployed URL: https://jonbullin84.github.io/PilotHub/
+![image](https://user-images.githubusercontent.com/105215596/180319870-25e391b5-0772-46e7-8507-e06b85822386.png)
---
+++ assets/js/beer.js
@@ -0,0 +1,69 @@
+var beerInfo = document.querySelector(".beer-info");
+
+
+// Pulls information of the nearest breweries which is linked to first API based off of latitude,longitude.
+function searchBeer(lat, long) {
+ // API for application or program
+  console.log(lat, long);
+  //API URL that returns results based off latitude and longitude in batches of 3
+  var getBeerInfo =
+    "https://api.openbrewerydb.org/breweries?by_dist=" +
+    lat +
+    "," +
+    long +
+    "&per_page=3";
+
+  fetch(getBeerInfo, options)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (response) {
+      console.log(response);
+    
+      // Clears out card for the following search
+      beerInfo.innerHTML = "";
+     // Iterates three times for brewery information
+      for(var i = 0; i < response.length; i++){
+
+      var brewery = document.createElement("div");
+//brewery name
+      var breweryName = document.createElement("h2");
+      breweryName.textContent = response[i].name;
+      console.log(response[i].name);
+      brewery.prepend(breweryName);
+//address
+      var address = document.createElement("div");
+      address.textContent = response[i].street;
+      brewery.append(address);
+      console.log(response[i].street);
+
+      var address2 = document.createElement("div");
+      address2.textContent =
+      response[i].city + ", " + response[i].state + " " + response[i].postal_code;
+      brewery.append(address2);
+//Phone Number
+      var phone = document.createElement("div");
+      phone.textContent = response[i].phone;
+      brewery.append(phone);
+//website
+      var website = document.createElement("div");
+      var link = document.createElement("a");
+      link.setAttribute("href", response[i].website_url);
+      link.setAttribute("target", "_blank");
+      link.textContent = response[i].website_url;
+      website.append(link);
+      brewery.append(website);
+
+      var brk = document.createElement("hr")
+      brewery.append(brk);
+
+      beerInfo.append(brewery);
+
+    }     
+})
+//error handling
+    .catch(function (error) {
+      console.log("error", error);
+    });
+}
+
